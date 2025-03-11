241f07e57fad88eb49be5fd7be8a9b676fb5c39d
Palces <zaira.anne.palces@accenture.com>
2025-03-11 14:29:38 +0800

    I add this ContactList

---
+++ src/components/ContactList.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const ContactList = ({ contacts, onDelete }) => (
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Phone</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      {contacts.map((contact) => (
+        <tr key={contact.id}>
+          <td>{contact.name}</td>
+          <td>{contact.email}</td>
+          <td>{contact.phone}</td>
+          <td>
+            <button onClick={() => onDelete(contact.id)}>Delete</button>
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+);
+
+export default ContactList;
